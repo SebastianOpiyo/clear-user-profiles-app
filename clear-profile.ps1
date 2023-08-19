f7dfa303621b3eef55dd00f1f390765c1f53997d
SebastianOpiyo <doka@DESKTOP-5G1L9IL.usiu.ac.ke>
2023-08-19 17:49:04 +0300

    FIX: Hive key and user profile display before delete.

--- clear-profile.ps1
+++ clear-profile.ps1
@@ -9,6 +9,19 @@ Write-Host "**************** ------- *****************"
 Write-Host ""
 Write-Host ""
 
+Write-Host "PROFILE CLEARNER SCRIPT"
+Write-Host "Created by Sebastian Opiyo - This is version 3 of the script"
+Write-Host "To Report Bugs, Contact: sebamopale@gmail.com"
+Write-Host "*************************************************************"
+
+Write-Host ""
+
+Write-Host "DELETE USER PROFILE FILES"
+Write-Host "Be careful with your choices, this can break the machine!"
+Write-Host "**********************************************************"
+
+Write-Host ""
+
 # Get the list of user profiles.
 $profiles = Get-WmiObject -Class Win32_UserProfile
 
@@ -27,23 +40,49 @@ if ($confirm -eq "Y") {
     foreach ($profile in $profiles) {
         # Get the path to the user profile directory.
         $profilePath = $profile.LocalPath
+        $profileSID = $profile.SID
 
         # Prompt the user to confirm the deletion of the user profile directory.
         $confirmDelete = Read-Host "Do you want to delete the user profile directory '$profilePath'? (Y/N)"
+        Write-Host "User profile SID: $($profileName)"
 
         # Delete the user profile directory if the user confirms.
         if ($confirmDelete -eq "Y") {
             Remove-Item -Path $profilePath -Force
-
-            $profileName = $profile.LocalPath.Substring($profile.LocalPath.LastIndexOf("\") + 1)
-
-            # Print the profile name
-            Write-Host "User profile: $($profileName)"
-
-            # Delete the registry key.
-            $registryPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$($profileName)"
-            Remove-Item -Path $registryPath
+            Write-Host "Path to user profile deleted successfuly."
         }
     }
 }
 
+
+# DELETE THE CORESPONDING CONTENTS IN THE REGISTRY
+# This script lists user profiles in the registry and asks for confirmation before deleting any profile.
+# It also deletes the profile permanently in the registry.
+Write-Host ""
+
+Write-Host "WE TOUCH THE HEART, THE HIVE"
+Write-Host "Be careful with your choices, this can break the machine!"
+Write-Host "**********************************************************"
+
+# Get the list of user profiles.
+$profiles = Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList" | Select-Object
+
+# Print the list of names and ProfileImagePath.
+# Print the list of names and ProfileImagePath.
+foreach ($hiveKey in $profiles) {
+    $profileImagePath = (Get-ItemProperty -Path ("HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\" + $hiveKey.PSChildName)).ProfileImagePath
+    Write-Host "Name: $($hiveKey.PSChildName)"
+    Write-Host "ProfileImagePath: $profileImagePath"
+
+    # Prompt the user to confirm the deletion of the registry key.
+    $confirmDelete = Read-Host "Do you want to delete the registry key '$($hiveKey.PSChildName)'? (Y/N)"
+
+    # Delete the registry key if the user confirms.
+    if ($confirmDelete -eq "Y") {
+        Remove-Item -Path ("HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\" + $hiveKey.PSChildName) -Force
+    }
+}
+
+Write-Host ""
+
+Write-Host "*****END OF SCRIPT****"
